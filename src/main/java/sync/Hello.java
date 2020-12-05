da60b4d1fcf1cba1fb05f081bb2df24d20a3d15f
zhangchuanxing <zhangchuanxing@idstaff.com>
2020-12-05 14:36:21 +0800

    lockSuppport 支持轮流打印字母

--- src/main/java/sync/Hello.java
+++ src/main/java/sync/Hello.java
@@ -6,8 +6,66 @@ package sync;
  * Description: 测试
  */
 public class Hello {
-    public static void main(String args[]) {
-        System.out.println("hello");
+    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    String[] letters = {"a", "b", "c", "c", "d", "e", "f", "g", "h", "i"};
+    String[] capitals = {"A", "B", "C", "C", "D", "E", "F", "G", "H", "I"};
 
+    private Object object = new Object();
+
+    public void testSynchronized() {
+        Thread thread1 = new Thread(() -> {
+            synchronized (object) {
+                for (int i = 0; i < numbers.length; i++) {
+                    System.out.println(numbers[i]);
+                    try {
+                        object.notify();
+                        object.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (object) {
+                for (int i = 0; i < letters.length; i++) {
+                    System.out.println(letters[i]);
+                    try {
+                        object.notify(); //叫醒其他线程，这里是t1
+                        object.wait(); //让自己阻塞，让出锁
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        });
+//
+//        Thread thread3 = new Thread(() -> {
+//            synchronized (object) {
+//                for (int i = 0; i < capitals.length; i++) {
+//                    System.out.println(capitals[i]);
+//                    try {
+//                        object.notify(); //叫醒其他线程，这里是t1
+//                        object.wait(); //让自己阻塞，让出锁
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+
+//        });
+        thread1.start();
+        thread2.start();
+//        thread3.start();
     }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.testSynchronized();
+    }
+
 }
---
+++ src/test/java/com/star/sync/LockSupportTest.java
@@ -0,0 +1,47 @@
+package com.star.sync;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Created by jk
+ * Create Date: 2020/12/5 11:52
+ * Description: 三个数组轮流打印字母  LockSupport版
+ */
+public class LockSupportTest {
+    static Thread thread2 = null;
+    static Thread thread3 = null;
+    static Thread thread1 = null;
+
+    public static void main(String args[]) {
+
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        String[] letters = {"a", "b", "c", "c", "d", "e", "f", "g", "h", "i"};
+        String[] capitals = {"A", "B", "C", "C", "D", "E", "F", "G", "H", "I"};
+
+        thread1 = new Thread(() -> {
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println(numbers[i]);
+                LockSupport.unpark(thread2);
+                LockSupport.park();
+            }
+        });
+        thread2 = new Thread(() -> {
+            for (int i = 0; i < letters.length; i++) {
+                LockSupport.park();
+                System.out.println(letters[i]);
+                LockSupport.unpark(thread3);
+            }
+        });
+        thread3 = new Thread(() -> {
+            for (int i = 0; i < capitals.length; i++) {
+                LockSupport.park();
+                System.out.println(capitals[i]);
+                LockSupport.unpark(thread1);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
