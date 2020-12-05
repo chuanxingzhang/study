9b3df254ddfc1c208200aa2f2365afc41f2d8abb
zhangchuanxing <zhangchuanxing@idstaff.com>
2020-12-05 11:56:00 +0800

    hello project

---
+++ src/main/java/sync/Hello.java
@@ -0,0 +1,13 @@
+package sync;
+
+/**
+ * Created by jk
+ * Create Date: 2020/12/5 11:52
+ * Description: 测试
+ */
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("hello");
+
+    }
+}
---
+++ src/test/java/com/star/sync/HelloTest.java
@@ -0,0 +1,13 @@
+package com.star.sync;
+
+/**
+ * Created by jk
+ * Create Date: 2020/12/5 11:52
+ * Description: 测试
+ */
+public class HelloTest {
+    public static void main(String args[]) {
+        System.out.println("hello");
+
+    }
+}
